80cf3b6509aef2a944a64d4ed72537661b99cc8b
lcruiz <luiscarlosieti@gmail.com>
2020-10-04 22:29:51 -0500

    mapa-completo

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
   public nameContinent:String = "";
-
+  public colors:string[] = ["darkgreen","darkorange","darkred"];
+  public carbonLevel:number = 15;
   constructor() { }
 
   ngOnInit(): void {
@@ -15,6 +16,21 @@ export class MapComponent implements OnInit {
   public continentClick(event):void{
     this.nameContinent = event.target.id
     console.log(event.target.id)
+  }
+  public mouseOver(event):void{
+    if(this.carbonLevel<=25){
+      let style = document.getElementById(event.target.id).style.fill=this.colors[0];
+    }
+    if(this.carbonLevel>25 && this.carbonLevel<=50){
+      let style = document.getElementById(event.target.id).style.fill=this.colors[1];
+    }
+    if(this.carbonLevel>50 && this.carbonLevel<=75){
+      let style = document.getElementById(event.target.id).style.fill=this.colors[2];
+    }
+         
+  }
+  public mouseLeave(event):void{
+    let style = document.getElementById(event.target.id).style.fill="skyblue";
   }
 
 }
