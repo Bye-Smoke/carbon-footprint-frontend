e9aa4b16899f9979868b793cbfa47de7f51183cb
Daniel <daquesada@unicauca.edu.co>
2020-10-04 23:26:43 -0500

    finish

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -6,31 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  public nameContinent:String = "";
-  public colors:string[] = ["darkgreen","darkorange","darkred"];
-  public carbonLevel:number = 15;
-  constructor() { }
+  public nameContinent: String = "";
+  public colors: string[] = ["darkgreen", "darkorange", "darkred"];
+  public carbonLevel: number = Math.random()*100;
+  temperature:number = Math.random()*100;
+  happy:boolean = false
+  medium:boolean = false
+  sad:boolean = false
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-  public continentClick(event):void{
+
+  public continentClick(event): void {
     this.nameContinent = event.target.id
-    console.log(event.target.id)
+    
+    // this.data.filter((value)=>)
   }
-  public mouseOver(event):void{
-    if(this.carbonLevel<=25){
-      let style = document.getElementById(event.target.id).style.fill=this.colors[0];
+  public mouseOver(event): void {
+    if (this.carbonLevel <= 25) {
+      this.happy=true
+      this.medium = false
+      this.sad = false
+      let style = document.getElementById(event.target.id).style.fill = this.colors[0];
     }
-    if(this.carbonLevel>25 && this.carbonLevel<=50){
-      let style = document.getElementById(event.target.id).style.fill=this.colors[1];
+    if (this.carbonLevel > 25 && this.carbonLevel <= 50) {
+      this.medium=true;
+      this.happy=false
+      this.sad = false
+      let style = document.getElementById(event.target.id).style.fill = this.colors[1];
     }
-    if(this.carbonLevel>50 && this.carbonLevel<=75){
-      let style = document.getElementById(event.target.id).style.fill=this.colors[2];
+    if (this.carbonLevel > 50 ) {
+      this.sad=true
+      this.happy=false
+      this.medium = false
+      let style = document.getElementById(event.target.id).style.fill = this.colors[2];
     }
-         
+    const f = async()=>{
+      this.carbonLevel = await Math.random()*100;
+      this.temperature = Math.random()*100;
+    }
+    f();
+    
   }
-  public mouseLeave(event):void{
-    let style = document.getElementById(event.target.id).style.fill="skyblue";
+  public mouseLeave(event): void {
+    let style = document.getElementById(event.target.id).style.fill = "skyblue";
   }
 
 }
--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Observer, Subscription } from 'rxjs';
 import { DataService } from '../services/data.service';
 
+interface Continent{
+  name,
+  temperature,
+  mtco2
+}
+
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
@@ -9,29 +15,40 @@ import { DataService } from '../services/data.service';
 })
 export class StatsComponent implements OnInit {
   stats: any;
-  subscription: Subscription;
-  observer: Observer<any>;
+  // subscription: Subscription;
+  // observer: Observer<any>;
   loading: Boolean = false;
-  error: any = ""
+  // error: any = ""
+  continent:Array<Continent> = new Array<Continent>()
   constructor(private data: DataService) {
-    this.observer = {
-      next: (data) => {
-        this.loading = true;
-        this.stats = data;
-        this.loading = false;
-      },
-      error: (error) => {
-        this.error = error
-      },
-      complete: () => { }
+    let names = ['Africa','America','Oceania','Europe','Asian']
+    for (let index = 0; index < names.length; index++) {
+      let continent:Continent = {
+        name:names[index],
+        mtco2:Math.random()*100,
+        temperature:Math.random()*100,
+      }
+      this.continent.push(continent);
+      
     }
+    // this.observer = {
+    //   next: (data) => {
+    //     this.loading = true;
+    //     this.stats = data;
+    //     this.loading = false;
+    //   },
+    //   error: (error) => {
+    //     this.error = error
+    //   },
+    //   complete: () => { }
+    // }
   }
 
   ngOnInit(): void {
-    this.subscription = this.data.getData().subscribe(this.observer)
+    // this.subscription = this.data.getData().subscribe(this.observer)
   }
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe()
-  }
+  // ngOnDestroy(): void {
+  //   this.subscription.unsubscribe()
+  // }
 
 }
